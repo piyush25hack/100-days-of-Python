9cde71d629f0b887fcf1b92c25fe74cac03a14f2
Piyush Namdev <piyushnamdev2015@gmail.com>
2026-03-30 01:18:11 +0530

    Day5_Password_Generator.py

--- Day5_Password Generator.py
+++ Day5_Password Generator.py
@@ -7,14 +7,29 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
-password=""
 
+# password=""
+# for char in range (0, nr_letters):
+#    password+=random.choice(letter)
+# for char in range (0, nr_symbols):
+#    password+=random.choice(symbols)
+# for char in range (0, nr_numbers):
+#    password+=random.choice(numbers)
+
+# print(password)
+
+password_list=[]
 for char in range (0, nr_letters):
-   password+=random.choice(letter)
+   password_list.append(random.choice(letter))
 for char in range (0, nr_symbols):
-   password+=random.choice(symbols)
+   password_list.append(random.choice(symbols))
 for char in range (0, nr_numbers):
-   password+=random.choice(numbers)
+   password_list.append(random.choice(numbers))
 
-print(password)
-
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+password=""
+for char in password_list:
+    password+=char
+print(f"Your password is: {password}")
