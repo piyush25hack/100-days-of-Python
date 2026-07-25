d5c0ee7d60a600e4579940059780068dbaf49cf9
Piyush Namdev <piyushnamdev2015@gmail.com>
2026-07-25 20:12:48 +0530

    Day_44_Motivation Meme

---
+++ Day43_Color Vocab Project/Test.py
@@ -0,0 +1,8 @@
+user = input("first name").lower()
+
+if(user == "piyush"):
+    print("Yes eligible for visa")
+elif (user == "james"):
+    print("Not Eligible")
+else:
+    print("Type your name")
